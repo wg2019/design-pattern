32742f4c47f84ee4f13f974e18cc3faf2be7369f
小刚刚 <41517731+wg2019@users.noreply.github.com>
2020-07-29 23:14:23 +0800

    工厂模式提交

---
+++ creation/factory/factory_test.go
@@ -0,0 +1,12 @@
+// Package factory 测试
+package factory_test
+
+import (
+	"github.com/wg2019/design-pattern/creation/factory"
+	"testing"
+)
+
+func TestFactoryPattern(t *testing.T) {
+	new(factory.AFactory).Manufacture().Show()
+	new(factory.BFactory).Manufacture().Show()
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/wg2019/design-pattern
+
+go 1.14
